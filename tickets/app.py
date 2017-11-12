461c77317c16006e0e6805ff981832bcc323fb9c
Karsten Jeschkies <jeskar@web.de>
2017-11-12 19:07:53 +0100

    Configure prod and PostgreSql database connection.

--- tickets/app.py
+++ tickets/app.py
@@ -1,7 +1,8 @@
 from flask import Flask
 import os
-from peewee import Model, SqliteDatabase
+from peewee import Model, PostgresqlDatabase, SqliteDatabase
 import stripe
+from urllib.parse import urlparse
 
 app = Flask(__name__)
 app.config.from_object('tickets.config.default')
@@ -18,7 +19,19 @@ stripe.api_key = stripe_keys['secret_key']
 
 class Database(object):
     def __init__(self, app):
-        self.db = SqliteDatabase(app.config['DB_NAME'])
+        if app.config['DB_ENGINE'] == 'peewee.SqliteDatabase':
+            self.db = SqliteDatabase(app.config['DB_NAME'])
+        elif app.config['DB_ENGINE'] == 'peewee.PostgresqlDatabase':
+            db_url = urlparse(os.environ.get('DATABASE_URL', None))
+            database = db_url.path[1:]
+            self.db = PostgresqlDatabase(
+                database,
+                user=db_url.username,
+                password=db_url.password,
+                host=db_url.hostname,
+                port=db_url.port)
+        self.db.connect()
+
         self.Model = self.get_model_class()
 
     def get_model_class(self):
